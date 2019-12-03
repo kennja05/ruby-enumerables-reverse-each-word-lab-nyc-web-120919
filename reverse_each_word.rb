154887b872b0a74b76020c8c812a3605da8b245a
Jacob Kenny <jacobkenny05@gmail.com>
2019-12-03 20:18:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ require 'pry'
 def reverse_each_word(string)
   final_array = []
   my_array = string.split(" ")
-  my_array.each do |b| 
+  my_array.each do |string| 
     final_array.push(string.reverse)
   end 
   binding.pry 
